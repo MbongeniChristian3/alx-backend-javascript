6a3a5c6e75c1e99910182392166319a5e7753205
MbongeniChristian3 <mbongenichristian3@gmail.com>
2024-09-30 19:17:46 +0000

    reports

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,18 @@
+export default function createReportObject(employeesList) {
+  // Create the allEmployees object using the spread syntax
+  const allEmployees = {
+    ...employeesList,
+  };
+
+  // Define the getNumberOfDepartments method
+  const getNumberOfDepartments = function() {
+    return Object.keys(allEmployees).length;
+  };
+
+  // Return the report object
+  return {
+    allEmployees,
+    getNumberOfDepartments,
+  };
+}
+
