6e451c94bdcda7faf81e8321ba5056fcc0adbad4
MbongeniChristian3 <mbongenichristian3@gmail.com>
2024-10-01 20:12:11 +0000

    promises1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,20 @@
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({
+                status: 200,
+                body: 'Success'
+            });
+        } else {
+            reject(new Error('The fake API is not working currently'));
+        }
+    });
+}
+
+getFullResponseFromAPI(true)
+    .then(response => console.log(response))
+    .catch(error => console.log(error.message));
+
+getFullResponseFromAPI(false)
+    .then(response => console.log(response))
+    .catch(error => console.log(error.message));
