c4b7a0a49466ddbcb3f27bfb4c90973c5b566885
MbongeniChristian3 <mbongenichristian3@gmail.com>
2024-10-01 20:00:13 +0000

    promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+      resolve("API response received successfully");
+    } else {
+      reject("Failed to fetch API response");
+    }
+  });
+}
+
