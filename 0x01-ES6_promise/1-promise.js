dd60fbb0d96725968c36f8e4145015b93689e732
MbongeniChristian3 <mbongenichristian3@gmail.com>
2024-10-04 13:00:11 +0000

    1-promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,20 +1,20 @@
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success'
-            });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success', // Added trailing comma here
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
 
 getFullResponseFromAPI(true)
-    .then(response => console.log(response))
-    .catch(error => console.log(error.message));
+  .then((response) => console.log(response)) // Added parentheses around parameter
+  .catch((error) => console.log(error.message)); // Added parentheses around parameter
 
 getFullResponseFromAPI(false)
-    .then(response => console.log(response))
-    .catch(error => console.log(error.message));
+  .then((response) => console.log(response)) // Added parentheses around parameter
+  .catch((error) => console.log(error.message)); // Added parentheses around parameter
